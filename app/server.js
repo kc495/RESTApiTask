f379b21ee67cb344f380e21f009b5b59465c6b0a
kc495 <kc495@canterbury.ac.uk>
2022-07-11 15:49:09 +0000

    DELETE the thing in the server

--- app/server.js
+++ app/server.js
@@ -4,7 +4,9 @@ const port = 8000
 
 app.use(express.json());
 
-const attendees = [
+ attendees = [
+  {"id": 123, "name": "tests", "notes": "somenotes"},
+  {"id": 223, "name": "tests", "notes": "somenotes"}
 
 ]
 
@@ -22,6 +24,13 @@ app.post('/attendee/', (req, res) => {
   attendees.push(req.body)
   res.status(201).json(req.body)
 })
+
+app.delete('/attendee/:id', (req, res) => {
+  console.log(" to be deleted ", req.params.id )
+  attendees = attendees.filter((i) => i.id !=req.params.id)
+  res.status(204).json({})
+}
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
